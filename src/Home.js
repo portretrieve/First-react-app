0f2884184f18c76dd75afd45a220c102fe3f2c0a
Devesh Kumar <deveshkumar2107@hotmail.com>
2021-06-02 00:35:49 -0400

    Final commit

--- src/Home.js
+++ src/Home.js
@@ -2,17 +2,14 @@ import BlogList from "./BlogList";
 import useFetch from "./useFetch";
 
 const Home = () => {
-
   const { data, ispending, error } = useFetch("http://localhost:8000/blogs");
 
   return (
     <div className="home">
-      {error && <div>{ error}</div>}
-      {ispending && <div>Loading....</div>}
+      {error && <div> {error} </div>} {ispending && <div> Loading.... </div>}
       {data && <BlogList blogsArray={data} title="All My Blogs!" />}
     </div>
   );
 };
 
 export default Home;
- 
